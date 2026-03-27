efd22bc307efe5da3ba2dcd0055a8276f4773f1a
JabinSultana <jabinasiff9926@gmail.com>
2026-03-27 12:17:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,40 +1,65 @@
-// Get buttons
-const buttons = document.querySelectorAll(".btn");
-
-// Get audio elements
-const audios = [
-    document.getElementById("audio1"),
-    document.getElementById("audio2"),
-    document.getElementById("audio3"),
-    document.getElementById("audio4"),
-    document.getElementById("audio5"),
-    document.getElementById("audio6")
+// Sound names
+const sounds = [
+    "applause",
+    "boo",
+    "gasp",
+    "tada",
+    "victory",
+    "wrong"
 ];
 
-// Stop all audio
+// Create audio elements dynamically
+sounds.forEach(sound => {
+
+    const audio = document.createElement("audio");
+
+    audio.src = "sounds/" + sound + ".mp3";
+    audio.id = sound;
+
+    document.body.appendChild(audio);
+
+});
+
+// Select buttons
+const buttons = document.querySelectorAll(".btn");
+
+// Stop all sounds
 function stopAllSounds() {
-    audios.forEach(audio => {
+
+    sounds.forEach(sound => {
+
+        const audio = document.getElementById(sound);
+
         audio.pause();
         audio.currentTime = 0;
+
     });
+
 }
 
-// Button click events
-buttons.forEach((button, index) => {
+// Add click events
+buttons.forEach(button => {
 
     button.addEventListener("click", () => {
 
-        // If stop button
+        // Stop button
         if (button.classList.contains("stop")) {
+
             stopAllSounds();
             return;
+
         }
+
+        // Get sound name
+        const soundName = button.innerText;
 
         // Stop others
         stopAllSounds();
 
-        // Play selected audio
-        audios[index].play();
+        // Play sound
+        const audio = document.getElementById(soundName);
+
+        audio.play();
 
     });
 
